9c34ae0ec59272e20ec2108bc4a6e411c21fb778
Danycm1 <danycmwow@gmail.com>
2021-09-21 12:24:04 +0200

    MVC: Add requirements Simplify flake8 command line Fix some pep8 issue in main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from controllers.menu_controller import MenuController
 
+
 def main():
     ''' Start the application '''
     MenuController().run_application()
+
 
 if __name__ == "__main__":
     main()
